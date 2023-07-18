ffa62e913ec2bf5f4e532ae671a830617ed825c6
vasu <vasu@gmail.com>
2023-07-18 10:48:22 +0530

    header add

---
+++ README.md
@@ -0,0 +1,11 @@
+##Lessons
+1. Introduction to the course
+2. Install development tools
+3. Create Angular App
+   1. Create project's folder
+   2. Install -g @angular/cli
+   3. Create App as frontend
+4. Add Header
+   1. Generate Component 
+   2. Add HTML
+   3. Add CSS
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './components/partials/headers/headers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent
   ],
   imports: [
     BrowserModule,
